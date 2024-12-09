3c9804d1da09b7548faf9abc3b40da8fb202d5b8
Hugo Luca Tigane <146342693+HugoLucaT@users.noreply.github.com>
2024-12-09 11:01:29 +0200

    trying

--- app/[city]/[movie]/page.tsx
+++ app/[city]/[movie]/page.tsx
@@ -1,24 +1,15 @@
 import Movie from "@/components/movie/movies";
 import SearchResult from "@/components/search/searchresult";
 
-export default function Index({
-  params,
-}: {
-  params: { city: string; movie: string };
-}) {
-  const { city, movie } = params;
+type Params = Promise<{ city: string; movie: string }>;
+
+export default async function Index(params: Params) {
+  const resolvedParams = await params;
+  const { city, movie } = resolvedParams;
 
   if (city === "search") {
-    return (
-      <>
-        <SearchResult movie={movie} />
-      </>
-    );
+    return <SearchResult movie={movie} />;
   } else {
-    return (
-      <>
-        <Movie movie={movie} city={city} />
-      </>
-    );
+    return <Movie movie={movie} city={city} />;
   }
 }
